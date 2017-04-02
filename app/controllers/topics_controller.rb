3812640a704cd1e98c7f05e2c561a0a17ecddec2
liubingjie <breeder1121@hotmail.com>
2017-04-02 16:29:16 +0800

    投票排序

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   # GET /topics
   # GET /topics.json
   def index
-    @topics = Topic.all
+    @topics = Topic.all.order('count desc')
   end
 
   # GET /topics/1
@@ -64,6 +64,7 @@ class TopicsController < ApplicationController
   def upvote
     @topic = Topic.find(params[:id])
     @topic.votes.create
+    @topic.update_attributes(count: @topic.votes.count)
     redirect_to(topics_path)
   end
 
@@ -72,7 +73,7 @@ class TopicsController < ApplicationController
     @topic.votes.first.destroy
     redirect_to(topics_path)
   end
-  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
